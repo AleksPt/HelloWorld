73e4703ad0f2e127fc10b478f6e2e0b88bbb2b17
Alex Petrenko <alex.pt@ya.ru>
2024-01-23 20:07:01 +0300

    fix

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -1,12 +1,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Private properties
+    private let firstView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemMint
+        return view
+    }()
+    
+    private let secondView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemPink
+        return view
+    }()
+    
+    // MARK: - Overrides Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBackground
+        view.addSubview(firstView)
+        view.addSubview(secondView)
+        
+        setConstraints()
     }
-
-
 }
 
+// MARK: - Extensions
+extension ViewController {
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            firstView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            firstView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            firstView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            firstView.bottomAnchor.constraint(equalTo: view.centerYAnchor, constant: -8),
+            
+            secondView.topAnchor.constraint(equalTo: view.centerYAnchor, constant: 8),
+            secondView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            secondView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            secondView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16)
+        ])
+    }
+}
